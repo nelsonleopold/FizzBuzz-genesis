c4866c9a557175224ab4515b28e9ba3f34b85f2a
nelsonleopold <nelsonleopold@gmail.com>
2022-04-11 10:36:44 -0700

    js section of fizzbuzz

--- js/site.js
+++ js/site.js
@@ -1,30 +1,60 @@
 // Step One, get the information (controller, accept requests)
-function getMessage() {
-    let msg = document.getElementById("txtMessage").value;
+function getValues() {
+    // First get the fizz and buzz values from user
+    fizzValue = document.getElementById("fizzValue").value;
+    buzzValue = document.getElementById("buzzValue").value;
 
-    displayMessage(msg);
+    // Call display values and pass fizz and buzz values to function
+    displayValues(fizzValue, buzzValue);
 }
 
 
 
 // Final Step, display the information (view, displays stuff on screen)
-function displayMessage(message) {
+function displayValues(fizz, buzz) {
+    // convert input strings to ints
+    let fizzValue = parseInt(fizz);
+    let buzzValue = parseInt(buzz);
 
+    // clear previous results when button is pressed more than once
+    clearResults();
 
-    // Bootstrap numbered list format: <li class="list-group-item">A list item</li>
-    // let item = `<li class="list-group-item">${message}</li>`;
-
-    // first get the ol element from the page
+    // get element where we want results to print
     element = document.getElementById("results");
 
-    // next create a new li element
-    let item = document.createElement("li");
+    // display a list of numbers from 1 to 100
+    // write a for loop to display values
+    for (let i = 1; i <= 100; i++) {
+        // create a new div element to be added to results
+        let item = document.createElement("div");
 
-    // add classes to li element
-    item.classList.add("list-group-item");
+        if (i % fizz == 0 && i % buzz == 0) {
+            item.innerHTML = 'FizzBuzz';
+        } else if (i % fizz == 0) {
+            item.innerHTML = 'Fizz';
+        } else if (i % buzz == 0) {
+            item.innerHTML = 'Buzz';
+        } else {
+            item.innerHTML = i;
+        }
 
-    // set the message for the li element
-    item.innerHTML = message;
+        // add class to div element
+        item.classList.add("col");
 
-    element.appendChild(item);
+        // add element to results
+        element.appendChild(item);
+    }
+
+    // for each number test if divisible by fizz and buzz
+    // if true, replace number with 'FizzBuzz'
+
+    // for each number test if divisible by fizz
+    // if true, replace number with 'Fizz'
+
+    // for each number test if divisible by buzz
+    // if true, replace number with 'Buzz'
+}
+
+function clearResults() {
+    document.getElementById("results").innerHTML = '';
 }
